28a826f4db052f4b50dd7acfb5721db860d7cb07
Andrea Gueugnaut <agueugnaut@octo.com>
2017-05-10 17:54:02 +0200

    feat(match): init match tests

---
+++ api/src/api/match/model.test.js
@@ -0,0 +1,11 @@
+import test from 'ava';
+
+test.todo('Create a match');
+
+test.todo('Find a match');
+
+test.todo('Find a match by id');
+
+test.todo('Update a match');
+
+test.todo('Delete a match');
