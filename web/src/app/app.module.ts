bdfdb138458cbb6edc36b418606c667734a47e58
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2017-05-11 08:47:19 +0200

    feat(app): add match module to app module

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MatchModule } from './match';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    MatchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
