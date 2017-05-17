3efbb80216a1832889530700cdbc1a018ffb7789
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2017-05-17 15:51:18 +0200

    feat(api): update team model

--- api/src/api/team/model.js
+++ api/src/api/team/model.js
@@ -1,6 +1,28 @@
-import { createVertex } from '../../services/arango/vertex';
+import { createVertexCollection } from '../../services/arango/vertex';
 
 export const TEAM_COLLECTION = 'teams';
 
-const Team = createVertex(TEAM_COLLECTION);
-export default Team;
+export const teamCollection = async () => {
+  const Team = await createVertexCollection(TEAM_COLLECTION);
+  Team.getOrSave = getOrSave(Team);
+  // Other custom methods
+  // ...
+  return Team;
+}
+
+/**
+ * Retrieves a team if it exists, or creates it.
+ */
+const getOrSave = (Team) => (team) => {
+  return Team.firstExample(team)
+    .catch(() => Team.firstExample({ players: team.players.reverse() }))
+    .catch(err => {
+      // Create a new team if none exist
+      if (err.code === 404) {
+        return Team.save(team).then(t => t.vertex);
+      } else {
+        // TODO: log error
+        console.log(err);
+      }
+    });
+};
