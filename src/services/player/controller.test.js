afd1c69ecb559e9f8c09dcc5dc239d126c84c612
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2017-08-02 17:12:15 +0200

    test(services): update player controller tests

--- src/services/player/controller.test.js
+++ src/services/player/controller.test.js
@@ -1,15 +1,41 @@
 import test from 'ava';
+import sinon from 'sinon';
 
-import createController, { find, get } from './controller';
+import createController, { find, findOneByTrigram } from './controller';
+
+const req = {
+  params: { trigram: 'ABC' }
+};
+const res = {};
+res.status = sinon.stub().returns(res);
+res.json = sinon.stub().returns(res);
+const next = sinon.stub();
 
 test('Should export default controller functions', async t => {
   t.is(typeof find, 'function');
-  t.is(typeof get, 'function');
+  t.is(typeof findOneByTrigram, 'function');
 });
 
 test('Should create a controller instance', async t => {
   const controller = createController({});
   t.is(typeof controller, 'object');
   t.is(typeof controller.find, 'function');
-  t.is(typeof controller.get, 'function');
+  t.is(typeof controller.findOneByTrigram, 'function');
+});
+
+test('Should find a player by trigram', async t => {
+  const model = {
+    findOne: sinon.stub().returns(Promise.resolve({
+      trigram: 'ABC'
+    }))
+  };
+  const controller = createController(model);
+  await controller.findOneByTrigram(req, res, next);
+  t.true(model.findOne.calledOnce);
+  t.true(res.status.calledOnce);
+  t.true(res.status.calledWithExactly(200));
+  t.true(res.json.calledOnce);
+  t.true(res.json.calledWithExactly({
+    trigram: 'ABC'
+  }));
 });
