70995441deff67e63435cc357370cf1b4bc2850b
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2017-08-02 18:45:39 +0200

    feat(services): add team and player routes to service index

--- src/services/index.js
+++ src/services/index.js
@@ -1,16 +1,21 @@
 import { Router } from 'express';
 
 import match from './match';
+import player from './player';
+import team from './team';
 
 export default (graph) => {
   const router = new Router();
+  // Create vertex collections
   const matchStore = graph.vertexCollection('matches');
-  const playedStore = graph.edgeCollection('played');
   const teamStore = graph.vertexCollection('teams');
+  const playerStore = graph.vertexCollection('players');
+  // Create edge collections
+  const playedStore = graph.edgeCollection('played');
 
   router.use(match(matchStore, playedStore, teamStore));
-  // router.use('/getTeams', team(arango));
-  // router.use(player(arango));
+  router.use(team(teamStore));
+  router.use(player(playerStore));
 
   return router;
 };
