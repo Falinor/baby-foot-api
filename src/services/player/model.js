3e6a0e708e57c97a08f54f44a109752f17533b85
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2017-08-02 17:13:45 +0200

    refactor(services): remove vertex and save methods from player model

--- src/services/player/model.js
+++ src/services/player/model.js
@@ -1,10 +1,8 @@
-import { find, findOne, vertex, save } from '../../components/model';
+import { find, findOne } from '../../components/model';
 
-export { find, findOne, vertex, save } from '../../components/model';
+export { find, findOne } from '../../components/model';
 
 export default (store) => ({
   find: find(store),
-  findOne: findOne(store),
-  vertex: vertex(store),
-  save: save(store)
+  findOne: findOne(store)
 });
--- src/services/player/model.test.js
+++ src/services/player/model.test.js
@@ -1,12 +1,10 @@
 import test from 'ava';
 
-import createModel, { find, findOne, vertex, save } from './model';
+import createModel, { find, findOne } from './model';
 
 test('Should export default model functions', async t => {
   t.is(typeof find, 'function');
   t.is(typeof findOne, 'function');
-  t.is(typeof vertex, 'function');
-  t.is(typeof save, 'function');
 });
 
 test('Should create a model instance', async t => {
@@ -14,6 +12,4 @@ test('Should create a model instance', async t => {
   t.is(typeof model, 'object');
   t.is(typeof model.find, 'function');
   t.is(typeof model.findOne, 'function');
-  t.is(typeof model.vertex, 'function');
-  t.is(typeof model.save, 'function');
 });
