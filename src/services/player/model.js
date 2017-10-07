5ddf38965279676c0f55a0975fa94c1c6d43a087
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2017-10-07 23:44:20 +0200

    test(services): add player model test

--- src/services/player/model.js
+++ src/services/player/model.js
@@ -3,13 +3,31 @@ import config from '../../config';
 
 /**
  * Find teams a player has been member of.
- * @param db {Object} The database instance.
+ * @param db {Object} A database instance.
  */
 export const findTeams = (db) => async (trigram) => {
   const query = `
     LET player = DOCUMENT(@id)
     FOR teams IN OUTBOUND player GRAPH @graph
-    RETURN teams
+      RETURN teams
+  `;
+  const bindVars = {
+    id: `${config.db.collections.players}/${trigram}`,
+    graph: config.db.graphName
+  };
+  return db.query(query, bindVars)
+    .then(cursor => cursor.all());
+};
+
+/**
+ * Find matches a player has been involved in.
+ * @param db {Object} A database instance.
+ */
+export const findMatches = (db) => async (trigram) => {
+  const query = `
+    LET player = DOCUMENT(@id)
+    FOR matches IN 2 OUTBOUND player GRAPH @graph
+      RETURN matches
   `;
   const bindVars = {
     id: `${config.db.collections.players}/${trigram}`,
@@ -27,6 +45,7 @@ export default (db) => {
   return {
     find: find(playerStore),
     findOne: findOne(playerStore),
-    findTeams: findTeams(db)
+    findTeams: findTeams(db),
+    findMatches: findMatches(db)
   };
 };
