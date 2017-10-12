107f6feb163ef234d43b72b72983db063bad5cda
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2017-10-12 21:04:34 +0200

    feat(services): add team model functions findPlayers and findMatches

--- src/services/team/model.js
+++ src/services/team/model.js
@@ -1,8 +1,41 @@
 import config from '../../config';
 import { find, save, vertex } from '../../components/model';
 
-export const getPlayers = (db) => async (teamHandle) =>
-  console.log('getPlayers');
+/**
+ * Find the matches that a team played.
+ * @param db {Object} The database instance.
+ */
+export const findMatches = (db) => (teamId) => {
+  const query = `
+    LET team = DOCUMENT(@id)
+    FOR matches IN OUTBOUND team GRAPH @graph
+      RETURN matches
+  `;
+  const bindVars = {
+    id: `${config.db.collections.teams}/${teamId}`,
+    graph: config.db.graphName
+  };
+  return db.query(query, bindVars)
+    .then(cursor => cursor.all());
+};
+
+/**
+ * Find the players belonging to a team.
+ * @param db {Object} The database instance.
+ */
+export const findPlayers = (db) => (teamId) => {
+  const query = `
+    LET team = DOCUMENT(@id)
+    FOR players IN INBOUND team GRAPH @graph
+      RETURN players
+  `;
+  const bindVars = {
+    id: `${config.db.collections.teams}/${teamId}`,
+    graph: config.db.graphName
+  };
+  return db.query(query, bindVars)
+    .then(cursor => cursor.all());
+};
 
 export { find, save, vertex } from '../../components/model';
 
@@ -11,7 +44,8 @@ export default (db) => {
     .vertexCollection(config.db.collections.teams);
   return {
     find: find(teamStore),
-    getPlayers: getPlayers(db),
+    findMatches: findMatches(db),
+    findPlayers: findPlayers(db),
     save: save(teamStore),
     vertex: vertex(teamStore)
   };
