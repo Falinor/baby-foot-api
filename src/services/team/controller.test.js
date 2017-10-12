ef250c33a6ce921b2e917e5c25fc01162a7317df
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2017-10-12 21:05:13 +0200

    test(services): add team controller functions findPlayers and findMatches

--- src/services/team/controller.test.js
+++ src/services/team/controller.test.js
@@ -1,39 +1,53 @@
 import test from 'ava';
 import sinon from 'sinon';
 
-import createController, { find, get, getMatches } from './controller';
+import createController, { create, find, get, findMatches } from './controller';
 
-const req = { params: { id: '1234' } };
-const res = {};
-res.status = sinon.stub().returns(res);
-res.json = sinon.stub().returns(res);
-const next = sinon.stub();
+test.beforeEach('Create the context', async t => {
+  const req = { params: { id: '1234' } };
+  const res = {};
+  res.status = sinon.stub().returns(res);
+  res.json = sinon.stub().returns(res);
+  const next = sinon.stub();
+  t.context = { req, res, next };
+});
 
 test('Should export default controller functions', async t => {
+  t.is(typeof create, 'function');
   t.is(typeof find, 'function');
+  t.is(typeof findMatches, 'function');
   t.is(typeof get, 'function');
-  t.is(typeof getMatches, 'function');
 });
 
 test('Should create a controller instance', async t => {
   const controller = createController({});
+  t.is(typeof controller.create, 'function');
   t.is(typeof controller.find, 'function');
+  t.is(typeof controller.findMatches, 'function');
   t.is(typeof controller.get, 'function');
-  t.is(typeof controller.getMatches, 'function');
 });
 
-test('Should get the matched that a team played', async t => {
+test('Should find the matches that a team played', async t => {
+  const { req, res, next } = t.context;
   const model = {
-    vertex: sinon.stub().resolves({
-      _id: '1234'
-    }),
-    getMatches: sinon.stub().resolves([
-      { red: { points: 10 }, blue: { points: 6 }, createdAt: new Date() }
-    ])
+    findMatches: sinon.stub().resolves([])
   };
   const controller = createController(model);
-  await controller.getMatches(req, res, next);
-  t.true(model.getMatches.calledOnce);
+  await controller.findMatches(req, res, next);
+  t.true(model.findMatches.calledOnce);
+  t.true(res.status.calledOnce);
+  t.true(res.status.calledWithExactly(200));
+  t.true(res.json.calledOnce);
+});
+
+test('Should find the players belonging to a team', async t => {
+  const { req, res, next } = t.context;
+  const model = {
+    findPlayers: sinon.stub().resolves([])
+  };
+  const controller = createController(model);
+  await controller.findPlayers(req, res, next);
+  t.true(model.findPlayers.calledOnce);
   t.true(res.status.calledOnce);
   t.true(res.status.calledWithExactly(200));
   t.true(res.json.calledOnce);
