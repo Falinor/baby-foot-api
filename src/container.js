33ea27022c387a1bc534e619560f4f0344f1ee21
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2018-01-30 08:44:37 +0100

    feat(container): add registrations to DI container

--- src/container.js
+++ src/container.js
@@ -1,19 +1,23 @@
-import { asFunction, createContainer, Lifetime } from 'awilix';
+import { asFunction, asValue, createContainer } from 'awilix';
+import bunyan from 'bunyan';
 
-import createGetPlayerUseCase from './app/player/get-player';
+import { createGetPlayerUseCase } from './app/player';
 import createPlayerRepository from './infra/player/repository';
 import createPlayerCtrl from './interfaces/http/player/controller';
+import createApp from './interfaces/http/app';
 
-export default () => createContainer()
-  // Use cases
-  .register('getPlayerUseCase', asFunction(createGetPlayerUseCase, {
-    lifetime: Lifetime.SINGLETON,
-  }))
+export default config => createContainer()
+  .register('config', asValue(config))
+  // Logger
+  .register('logger', asValue(bunyan.createLogger({
+    name: config.appName,
+    level: config.log.level,
+  })))
+  // Register application
+  .register('app', asFunction(createApp).singleton())
+  // Register use cases
+  .register('getPlayerUseCase', asFunction(createGetPlayerUseCase))
   // Repositories
-  .register('playerRepository', asFunction(createPlayerRepository, {
-    lifetime: Lifetime.SINGLETON,
-  }))
+  .register('playerRepository', asFunction(createPlayerRepository).singleton())
   // Controllers
-  .register('playerController', asFunction(createPlayerCtrl, {
-    lifetime: Lifetime.SINGLETON,
-  }));
+  .register('playerController', asFunction(createPlayerCtrl));
