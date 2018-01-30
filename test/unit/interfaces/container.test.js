ff7e562de1a91868e74f1924a7126f04e042b04e
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2018-01-30 08:44:58 +0100

    test(container): add registration tests

--- test/unit/interfaces/container.test.js
+++ test/unit/interfaces/container.test.js
@@ -2,27 +2,31 @@ import test from 'ava';
 
 import createContainer from '../../../src/container';
 
+// Create a one-time container
+const container = createContainer({
+  appName: 'test',
+  log: {
+    level: 'info',
+  },
+});
+
 test('Should create a container', async (t) => {
-  const container = createContainer();
   t.is(typeof container, 'object');
   t.is(typeof container.registrations, 'object');
   t.is(typeof container.registrations.playerController, 'object');
 });
 
-test('Should inject a GetPlayer use case', async (t) => {
-  const container = createContainer();
+test('Should resolve a GetPlayer use case', async (t) => {
   const getPlayerUseCase = container.resolve('getPlayerUseCase');
   t.is(typeof getPlayerUseCase, 'object');
 });
 
-test('Should inject a player repository', async (t) => {
-  const container = createContainer();
+test('Should resolve a player repository', async (t) => {
   const playerRepository = container.resolve('playerRepository');
   t.is(typeof playerRepository, 'object');
 });
 
-test('Should inject a player controller', async (t) => {
-  const container = createContainer();
+test('Should resolve a player controller', async (t) => {
   const playerCtrl = container.resolve('playerController');
   t.is(typeof playerCtrl, 'object');
 });
