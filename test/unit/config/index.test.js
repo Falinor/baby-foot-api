06d07955ab4612ed7eb71347a515e5a9d16ea2f3
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2018-01-30 08:46:05 +0100

    test(config): update config tests

--- test/unit/config/index.test.js
+++ test/unit/config/index.test.js
@@ -1,22 +1,16 @@
 import test from 'ava';
 
-import config from '../../../src/config/index';
+import createConfig from '../../../src/config/index';
 
 test('Should return the config object', async (t) => {
-  t.truthy(config.env);
-  t.truthy(config.root);
-  t.truthy(config.port);
-  t.truthy(config.ip);
-  t.truthy(config.db);
+  const config = createConfig({
+    DB_URL: 'mydb',
+  });
+  t.is(typeof config, 'object');
+  t.is(typeof config.appName, 'string');
+  t.is(typeof config.env, 'string');
+  t.is(typeof config.port, 'number');
   t.is(typeof config.db, 'object');
   t.is(typeof config.db.url, 'string');
-  t.is(typeof config.db.databaseName, 'string');
-  t.is(typeof config.db.graphName, 'string');
   t.is(typeof config.db.retryConnection, 'boolean');
-  t.is(typeof config.db.collections, 'object');
-  t.is(typeof config.db.collections.matches, 'string');
-  t.is(typeof config.db.collections.teams, 'string');
-  t.is(typeof config.db.collections.players, 'string');
-  t.is(typeof config.db.collections.played, 'string');
-  t.is(typeof config.db.collections.member, 'string');
 });
