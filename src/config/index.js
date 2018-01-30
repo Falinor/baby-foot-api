1365ff0f346a72572a320e694e92357b4e20af5f
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2018-01-30 08:43:37 +0100

    feat(config): review config

--- src/config/index.js
+++ src/config/index.js
@@ -1,34 +1,19 @@
-/* eslint-disable no-unused-vars */
-import path from 'path';
-
-/* istanbul ignore next */
-const requireProcessEnv = (name) => {
-  if (!process.env[name]) {
+const requireProcessEnv = (env, name) => {
+  if (!env[name]) {
     throw new Error(`You must set the ${name} environment variable`);
   }
-  return process.env[name];
+  return env[name];
 };
 
-export default {
-  env: process.env.NODE_ENV || 'development',
-  root: path.join(__dirname, '..', '..'),
-  port: process.env.PORT || 9000,
-  ip: process.env.IP || '0.0.0.0',
-  // masterKey: requireProcessEnv('MASTER_KEY'),
-  logLevel: process.env.LOG_LEVEL || 'debug',
-  db: {
-    url: process.env.DB_URL || 'http://localhost:8529',
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    databaseName: process.env.DB_NAME || 'BabyFootDB',
-    graphName: process.env.DB_GRAPH_NAME || 'BabyFootGraph',
-    retryConnection: true,
-    collections: {
-      matches: 'matches',
-      teams: 'teams',
-      players: 'players',
-      played: 'played',
-      member: 'member',
-    },
+export default (environment = process.env) => ({
+  appName: environment.APP_NAME || 'Baby-foot API',
+  env: environment.NODE_ENV || 'development',
+  port: environment.PORT || 9000,
+  log: {
+    level: environment.LOG_LEVEL || 'debug',
   },
-};
+  db: {
+    url: requireProcessEnv(environment, 'DB_URL'),
+    retryConnection: !!environment.DB_RETRY_CONNECTION || true,
+  },
+});
