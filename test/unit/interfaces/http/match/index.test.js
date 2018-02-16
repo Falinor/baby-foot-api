7389d5ec116fd16f666d27519f1c66731182a73b
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2018-02-16 22:29:31 +0100

    test(lint): override no-ignored-test-files rule

--- test/unit/interfaces/http/match/index.test.js
+++
@@ -1,7 +0,0 @@
-import test from 'ava';
-
-import match from '../../../../../src/interfaces/http/match';
-
-test.failing('Should create match routes', async (t) => {
-  t.is(typeof match, 'object');
-});
