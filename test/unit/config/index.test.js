9fb130a5c51b819d8e5ed03a64558d8f605368bf
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2018-02-17 09:26:54 +0100

    test(config): cover config with new tests

--- test/unit/config/index.test.js
+++ test/unit/config/index.test.js
@@ -2,16 +2,49 @@ import test from 'ava';
 
 import createConfig from '../../../src/config/index';
 
-test('Should return the config object', async (t) => {
-  const config = createConfig({
-    DB_URL: 'mydb',
-  });
+test('Should return a config object', (t) => {
+  const config = createConfig();
   t.is(typeof config, 'object');
-  t.is(typeof config.appName, 'string');
-  t.is(typeof config.host, 'string');
-  t.is(typeof config.env, 'string');
-  t.is(typeof config.port, 'number');
-  t.is(typeof config.db, 'object');
-  t.is(typeof config.db.url, 'string');
-  t.is(typeof config.db.retryConnection, 'boolean');
+  t.deepEqual(config, {
+    appName: 'Baby-foot API',
+    env: 'development',
+    host: 'http://localhost',
+    port: 9000,
+    log: {
+      level: 'debug',
+    },
+    db: {
+      url: 'mongodb://localhost:27017',
+      name: 'baby-foot',
+      retryConnection: true,
+    },
+  });
+});
+
+test('Should return a completely customized config object', (t) => {
+  const opts = {
+    APP_NAME: 'myCoolAppName',
+    NODE_ENV: 'myCoolEnv',
+    HOST: 'myCoolHost',
+    PORT: '8000',
+    LOG_LEVEL: 'myLogLevel',
+    DB_URL: 'myDBURL',
+    DB_NAME: 'myDBName',
+    DB_RETRY_CONNECTION: 'true',
+  };
+  const config = createConfig(opts);
+  t.deepEqual(config, {
+    appName: 'myCoolAppName',
+    env: 'myCoolEnv',
+    host: 'myCoolHost',
+    port: 8000,
+    log: {
+      level: 'myLogLevel',
+    },
+    db: {
+      url: 'myDBURL',
+      name: 'myDBName',
+      retryConnection: true,
+    },
+  });
 });
