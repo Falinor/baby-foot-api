e937a99c3cef0fcba295daedc2fff3afa16d0ead
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2018-03-12 23:00:52 +0100

    test(player): implement repository tests

--- test/unit/infra/player/repository.test.js
+++ test/unit/infra/player/repository.test.js
@@ -1,13 +1,46 @@
 import test from 'ava';
 import sinon from 'sinon';
 
-import createRepository from '../../../../src/infra/player/repository';
+import createRepository, {
+  toDBO,
+  toEntity,
+} from '../../../../src/infra/player/repository';
+import { inputPlayer } from '../../../data';
 
-test('Should create a player repository', async (t) => {
+test('Should create a player repository', (t) => {
   const repo = createRepository();
   t.is(typeof repo, 'object');
+  t.is(typeof repo.create, 'function');
   t.is(typeof repo.find, 'function');
   t.is(typeof repo.findById, 'function');
+});
+
+test('Should convert an entity to a DBO', (t) => {
+  const playerEntity = {
+    id: 42,
+  };
+  const playerDBO = toDBO(playerEntity);
+  t.deepEqual(playerDBO, {
+    _id: playerEntity.id,
+  });
+});
+
+test('Should convert a DBO to an entity', (t) => {
+  const playerDBO = {
+    _id: 1337,
+  };
+  const playerEntity = toEntity(playerDBO);
+  t.deepEqual(playerEntity, {
+    id: 1337,
+  });
+});
+
+test('Should create a player', async (t) => {
+  const insertOne = sinon.stub().resolves(inputPlayer);
+  const repo = createRepository({ insertOne });
+  await repo.create(inputPlayer);
+  t.true(insertOne.calledOnce);
+  t.true(insertOne.calledWithExactly(inputPlayer));
 });
 
 test('Should find players', async (t) => {
